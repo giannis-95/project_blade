8d12aef82b9a5d809bf9c429da8d419ff2f5ac49
giannis-95 <70375540+giannis-95@users.noreply.github.com>
2024-02-12 19:41:30 +0200

    changes model and table users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,9 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'email',
         'password',
-        'firstname' ,
-        'lastname' ,
-        'phone'
+        'name' ,
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('phone');
+            $table->string('name');
             $table->rememberToken();
             $table->timestamps();
         });
